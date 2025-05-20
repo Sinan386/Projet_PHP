6d6ed47107b57c0d063f8a69ab1f86df4a38ac8c
Sinan Demiroz <ton@email.com>
2025-05-20 09:46:20 +0200

    Création item.php avec une image

---
+++ item.php
@@ -0,0 +1,15 @@
+<?php 
+
+$produit = "Patek Philippe Grandmaster Chime 5175R";
+$prix = 2500000;
+$image = "https://magmontres.fr/wp-content/uploads/2014/10/TheGrandmasterChime-1.jpg";
+
+echo "<h1>$produit</h1><br>";
+echo "<h2><p>Prix : $prix €</p><h2>";
+echo '<img src="' . $image . '" alt="Montre de luxe">';
+
+
+?>
+
+
+
